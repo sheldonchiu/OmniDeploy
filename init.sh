1dc2cc8ffb422d57b74b4c9bc6589370c9ca0248
sheldonchiu <sheldon.chiu1998@gmail.com>
2025-12-06 12:58:49 +0800

    test

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# docker run -it --rm -v /root:/data -p 8888:8888 -v /mnt/data/OmniDeploy:/workspace --gpus all nvidia/cuda:13.0.2-cudnn-devel-ubuntu24.04 /bin/bash -c "apt-get -qq update && apt-get install -y curl > /dev/null && curl -s https://raw.githubusercontent.com/sheldonxxxx/OmniDeploy/refs/heads/dev/init.sh | bash && /bin/bash"
+# docker run -it --rm -v /root:/data -p 8888:8888 -v /mnt/data/OmniDeploy:/workspace --gpus all omnideploy /bin/bash -c "apt-get -qq update && apt-get install -y curl > /dev/null && curl -s https://raw.githubusercontent.com/sheldonxxxx/OmniDeploy/refs/heads/dev/init.sh | bash && /bin/bash"
 set -e
 
 # Update and install dependencies with reduced verbosity
@@ -12,7 +12,9 @@ mkdir -p "$WORKING_DIR"
 
 # Clone and run OmniDeploy
 cd "$WORKING_DIR"
-git clone -q https://github.com/sheldonxxxx/OmniDeploy.git > /dev/null
+if [[ ! -d "OmniDeploy" ]]; then
+  git clone -q https://github.com/sheldonxxxx/OmniDeploy.git > /dev/null
+fi
 cd "$WORKING_DIR/OmniDeploy"
 git checkout dev
 bash entry.sh
